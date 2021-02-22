56773ca9ea3f0b8916143703a0da4c904096d945
jamesAforster <jamesalexforster@gmail.com>
2021-02-22 21:11:22 +0000

    Adds film model

---
+++ ArtLog/Models/Film.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ArtLog.Models
+{
+    public class Film
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Id { get; set; }
+    }
+}
