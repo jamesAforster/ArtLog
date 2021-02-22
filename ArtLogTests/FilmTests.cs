aa28a00e88d506041fda3f1b15dc9ae8ef3c4e5f
jamesAforster <jamesalexforster@gmail.com>
2021-02-22 21:18:31 +0000

    Adds FilmTests

---
+++ ArtLogTests/FilmTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace ArtLogTests
+{
+    public class FilmTests
+    {
+        [Fact]
+        public void Film_Has_Correct_Data_Structure()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
