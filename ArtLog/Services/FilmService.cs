45285d2f35cb897f70b7ccbe09834ae7f635be86
jamesAforster <jamesalexforster@gmail.com>
2021-02-26 16:44:37 +0000

    Adds argument to GetFilm()

--- ArtLog/Services/FilmService.cs
+++ ArtLog/Services/FilmService.cs
@@ -7,9 +7,8 @@ namespace ArtLog.Services
 {
     public class FilmService
     {
-        public async Task<HttpResponseMessage> GetFilm()
+        public async Task<HttpResponseMessage> GetFilm(string query)
         {
-            string query = "James";
             string searchQuery = CreateURIQuery(query);
             var client = new HttpClient();
             var request = new HttpRequestMessage
@@ -17,6 +16,7 @@ namespace ArtLog.Services
                 Method = HttpMethod.Get,
                 RequestUri = new Uri($"https://movie-database-imdb-alternative.p.rapidapi.com/?s={searchQuery}&page=1&r=json"),
                 Headers =
+
             {
                 { "x-rapidapi-key", "b3d70bb793msh4e53df863c80e1ap13c999jsn53d5c3a65ae6" },
                 { "x-rapidapi-host", "movie-database-imdb-alternative.p.rapidapi.com" },
